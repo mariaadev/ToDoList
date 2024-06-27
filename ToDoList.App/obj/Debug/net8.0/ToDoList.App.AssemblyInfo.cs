2e2134a8940854195b9cf9e0d4e23bdfad888428
Maria <mini.mar@hotmail.com>
2024-06-27 21:49:50 +0200

    adding method to load tasks async after adding task

--- ToDoList.App/obj/Debug/net8.0/ToDoList.App.AssemblyInfo.cs
+++ ToDoList.App/obj/Debug/net8.0/ToDoList.App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b68b76d93ccd5818421a40f2131b5fab3f7bae03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f5f23a060360c8132b2719b9aa6ef48af32782f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
